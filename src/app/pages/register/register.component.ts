c2a5ae4c26e3484014f8f1536c632cd956b89faa
naopeke <naopeke@protonmail.com>
2024-06-20 17:37:54 +0200

    register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  protected registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  public onSubmit(){
+    if (this.registerForm.valid){
+      console.log(this.registerForm.value);
+      this.authService.register(this.registerForm.value).subscribe({
+        next: (data: any) => {
+          console.log(data);
+          this.router.navigate(['/login']);
+        },
+        error: (err) => console.log('Error onSubmit', err)
+      });
+    }
+  }
 
 }
