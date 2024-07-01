5664308b51d5ce8a31160b9a841fba8369793740
naopeke <naopeke@protonmail.com>
2024-07-01 20:29:10 +0200

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
