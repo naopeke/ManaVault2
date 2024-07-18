1df2abc1d630ce0c222bbb89cacd199baade9d08
naopeke <naopeke@protonmail.com>
2024-07-18 10:03:23 +0200

    sidebar2

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RippleModule],
+  imports: [],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
